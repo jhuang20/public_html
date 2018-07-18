976eeabc2148be200bda07fa8c565a072eab5207
James Huang <33487663+jhuang20@users.noreply.github.com>
2018-07-18 17:40:02 -0400

    Create remove.py

---
+++ CitiBike/remove.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+import cgi,cgitb,os
+cgitb.enable()
+form=cgi.FieldStorage()
+print """Content-type: text/html\n\n"""
+print
+#this creates a new page!
+def FStoD():
+    '''
+    Converts cgi.FieldStorage() return value into a standard dictionary
+    '''
+    d = {}
+    formData = cgi.FieldStorage()
+    for k in formData.keys():
+        d[k] = formData[k].value
+    return d
+
+def remove():
+    file=FStoD()['file']
+    os.remove(file+".csv")
+    os.remove(file+"-admin.csv")
+    os.remove(file+".py")
+
+def user():
+    return """
+    <!DOCTYPE html>
+<html>
+<head>
+  <link rel="shortcut icon" type="image/png" href="logo.png" />
+</head>
+<title>Dashboard</title>
+<body>
+<h1>U submitted an event!</h1>
+<p><a href="dashboard.py">back to dashboard</a></p>
+</body>
+</html>"""
+remove()
+print user()
