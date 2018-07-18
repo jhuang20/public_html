365b50f044f276e5cd311d7d611cbcaae569dd7d
James Huang <33487663+jhuang20@users.noreply.github.com>
2018-07-18 17:26:08 -0400

    Update dashboard.py

--- CitiBike/dashboard.py
+++ CitiBike/dashboard.py
@@ -19,7 +19,7 @@ def getEvent():#gets event DATE
 <div class="w3-col m3 container w3-text-black">
         <a href="""+ping+""".py>"""+ret+"""</a>
         <form action="remove.py">
-<input type="hidden" id="file" name="file" value='"""+ping+"""'>
+<input type="hidden" id="file" name="file" value='"""+str(ping)+"""'>
   <input type="submit" value="Delete Event">
 </form></div>
         """
