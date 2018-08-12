22013ca6e444811d19dfbd621cbc2c708c5b5042
James Huang <33487663+jhuang20@users.noreply.github.com>
2018-08-12 10:21:46 -0400

    Update signup.py

--- CitiBike/signup.py
+++ CitiBike/signup.py
@@ -272,6 +272,16 @@ body, html {
 <script>
 </script>
 <script>
+// Change style of navbar on scroll
+window.onscroll = function() {myFunction()};
+function myFunction() {
+    var navbar = document.getElementById("myNavbar");
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        navbar.className = "w3-bar" + " w3-card" + " w3-animate-top" + " w3-white";
+    } else {
+        navbar.className = navbar.className.replace(" w3-card w3-animate-top w3-white", "");
+    }
+}
 function toggleFunction() {
     var x = document.getElementById("navDemo");
     if (x.className.indexOf("w3-show") == -1) {
