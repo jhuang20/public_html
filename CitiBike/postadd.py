7f87ab36a3956c4dac812334c0cc956e26e9bf8a
James Huang <33487663+jhuang20@users.noreply.github.com>
2018-08-24 16:19:16 -0400

    Update postadd.py

--- CitiBike/postadd.py
+++ CitiBike/postadd.py
@@ -124,17 +124,31 @@ def getevent(): #retreives event data
     final=' '
     count=0
     countAcceptance=0
+    final+="<form action='duplicate.py'><input type='hidden' id='file' name='file' value='2018-08-27'><input type='date' name='newdate'><input type='submit' value='Reschedule'></form>"
+    final+="<form action='close.py'><input type='hidden' id='file' name='file' value='2018-08-27'><input type='submit' value='Close Form'></form>"
+    final+="<form action='confirm.py'><input type='hidden' id='file' name='file' value='2018-08-27'><input type='submit' value='View Roster'></form>"
+    count=0
+    countAcceptance=0
+    countConfirm=0
+    countWaitlist=0
     for j in lines:
 	count+=1
-	isAccept=j.split(",")
-	if isAccept[-1]=="accept /n": #counts number of acceptances(should use strip function )
+	meow=j.split(",")
+	if str(meow[-1])=="accept \n":
 	    countAcceptance+=1
+	if str(meow[-1])=="confirm \n":
+	    countConfirm+=1
+	    final+=str(meow[3])+"<br>"
+	if str(meow[-1])=="waitlist \n" or str(meow[-1])=="waitlist\n":
+	    countWaitlist+=1
     final+="<p><b>"+str(count)+ "</b> people are signed up</p>"
-    if '"""+FStoD()['type']+"""'!='safetyclass':
-        final+="<p><b>"+str(count-"""+FStoD()['size']+""")+"</b> people are on waitlist</p>" #lists number of people who are signup up and waitlisted
+    if '"""+FStoD()['type']+"""'=='groupride':
+	final+="<p><b>"+str(count-"""+FStoD()['size']+""")+"</b> people are on waitlist</p>" #lists number of people who are signup up and waitlisted
     final+="<p>Remove Person(enter OSIS):<form action='rmperson.py'><input type='text' name='osis'><input type='hidden' id='file' name='file' value='"""+det(FStoD()['date'])+"""'><input type='submit' value='deleteperson'></form></p>"
     if '"""+FStoD()['type']+"""'=='safetyclass' or '"""+FStoD()['type']+"""'=='specialevent':
+        final+="<p><b>"+str(countWaitlist)+"</b> people have been waitlisted</p>"
 	final+="<p><b>"+str(countAcceptance)+" people have been accepted</b></p>"
+	final+="<p><b>"+str(countConfirm)+"</b> people have been CONFIRMED</p>"
 	final+="<p>Accept person(Enter OSIS):<form action='accept.py'><input type='text' name='osis'><input type='hidden' id='file' name='file' value='"""+det(FStoD()['date'])+"""'><input type='hidden' name='admit' value='1'><input type='submit' value='accept'></form></p>"
         final+="<p>Reject Person(Enter OSIS):<form action='accept.py'><input type='text' name='osis'><input type='hidden' id='file' name='file' value='"""+det(FStoD()['date'])+"""'><input type='hidden' name='admit' value='0'><input type='submit' value='reject'></form></p>"
     count=0 #the three lines above provide the ability to delete a person, accept a person, or reject a person. 
