82ed0252ccd218071c1c8c9da15228964e286d50
James Huang <33487663+jhuang20@users.noreply.github.com>
2018-08-27 15:49:48 -0400

    Update postadd.py

--- CitiBike/postadd.py
+++ CitiBike/postadd.py
@@ -477,24 +477,18 @@ Email: <input name="contact" required="required"></input><br>"""+form(FStoD()['t
 </form>
 </div>
 <script>
-// Change style of navbar on scroll
-window.onscroll = function() {myFunction()};
 function myFunction() {
-    var navbar = document.getElementById("myNavbar");
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-        navbar.className = "w3-bar" + " w3-card" + " w3-animate-top" + " w3-white";
+    var x = document.getElementById("myNavbar");
+    if (x.className === "topnav") {
+        x.className += " responsive";
     } else {
-        navbar.className = navbar.className.replace(" w3-card w3-animate-top w3-white", "");
+        x.className = "topnav";
     }
 }
-function toggleFunction() {
-    var x = document.getElementById("navDemo");
-    if (x.className.indexOf("w3-show") == -1) {
-        x.className += " w3-show";
-    } else {
-        x.className = x.className.replace(" w3-show", "");
-    }
-}
+</script>
+<script>
+// Change style of navbar on scroll
+
 </script>
         </body>
 	
