d3f58fdfa1ecf8671f1920e1250b3960d4b13519
junhu <jhuang20@stuy.edu>
2018-08-28 17:33:38 -0400

    added new blog features

--- CitiBike/eventadd.py
+++ CitiBike/eventadd.py
@@ -17,14 +17,16 @@ def FStoD():
     for k in formData.keys():
         d[k] = formData[k].value
     return d
-
+def addFormat(ting):
+    ting=ting.replace("\n","</p><br><p>")
+    return ting
 def newCode():
     title=FStoD()['title']
     content=FStoD()['content']
     tag=FStoD()['tag']
     tiempo= datetime.datetime.now()
-    return "<div id='"+tag+"'> <h3 class='w3-center'>"+title+"</h3><br><p id='body' class='w3-left'>"+content+"""</p>
-      <br><p class="w3-small w3-left">"""+str(tiempo.strftime("%Y-%m-%d %H:%M"))+"</p></div><br><br><br>"
+    return "<div id='"+tag+"'> <h3 class='w3-center'>"+title+"</h3><br><p id='body' class='w3-left'>"+addFormat(content)+"""</p>
+      <br><p class="w3-small w3-left">"""+str(tiempo.strftime("%Y-%m-%d %H:%M"))+"</p></div>"
 def addToBlog():
     fd = open('blog.html','r+')
     icon=fd.read()
@@ -35,7 +37,14 @@ def addToBlog():
     hey=header+newCode()+footer
     fd.write(hey)
     fd.close()
-
+def addPost():
+    now=datetime.datetime.now()
+    file_path=str(now.strftime("%Y-%m-%d")).replace("-","/")
+    if not os.path.exists(file_path):
+	os.makedirs(file_path)
+    fd= open(str(file_path)+"/"+FStoD()['title'].replace(" ","")+".html", 'w')
+    fd.write("<html><body>"+newCode()+"</body></html>")
+    fd.close()
 
 def user():
     return """
@@ -48,11 +57,11 @@ def user():
 
 <body>
 
-<h1>You submitted a blog post!</h1>
+<h1>You submitted a blog post! It resides at this location:</h1>
 <p><a href="dashboard.py">back to dashboard</a></p>
 
 </body>
 </html>"""
-addToBlog()
 #csvWrite()
+addPost()
 print user()
